779fdfd782baa030aec48c7a20ab7f84c367732a
YangZheYu1219 <ji3g420132@yahoo.com.tw>
2022-05-11 12:04:56 +0800

    增加gitignore, cowsay

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const hello = require('./hello');
+//第一個區塊 內建模組
+const path = require('path');
+const http = require('http');
 
+//第二個區塊 第三方模組(套件)
+const cowsay = require('cowsay');
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+
+//第三個區塊 自建模組
+const hello = require('./hello');
 hello.sayHello();
 hello.sayGoodnight();
 console.log(hello.title)
+
+/////////////////////////////////////////////
